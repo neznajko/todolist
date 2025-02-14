b4e96c9e30ab70b504a74ae2df4e41df9044c096
neznajko <neznajko@yahoo.com>
2025-02-14 18:45:22 +0200

    fyorst commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////
+using todolist.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext <TodolistContext> ();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapRazorPages();
+app.Run();
+
+////////////////////////////////////////////////////////////////
