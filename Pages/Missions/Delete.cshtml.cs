b457ebd5af7a0e6d9c6a9a88e0f4c888030f4637
neznajko <neznajko@yahoo.com>
2025-02-27 18:21:32 +0200

    Missions

--- Pages/Missions/Delete.cshtml.cs
+++ Pages/Missions/Delete.cshtml.cs
@@ -9,24 +9,26 @@ namespace todolist.Pages.Missions;
 ////////////////////////////////////////////////////////////////
 public class DeleteModel: PageModel {
     private readonly TodolistContext context;
-    public DeleteModel(TodolistContext context) {
+    public DeleteModel( TodolistContext context ){
         this.context = context;
     }
     [BindProperty]
     public Mission Mission { get; set; }
-    public async Task<IActionResult> OnGetAsync(int id) {
-        Mission = await context.Missions.FindAsync(id);
+    public async Task <IActionResult> OnGetAsync( int id ){
+        Mission = await context.Missions.FindAsync( id );
         return Mission == null ? NotFound() : Page();
     }
-    public async Task<IActionResult> OnPostAsync(int id) {
-        Mission = await context.Missions.FindAsync(id);
+    public async Task <IActionResult> OnPostAsync( int id ){
+        Mission = await context.Missions.FindAsync( id );
         if( Mission == null ) return NotFound();
         int operationId = Mission.OperationId;
-        context.Missions.Remove(Mission);
+        context.Missions.Remove( Mission );
         await context.SaveChangesAsync();
-        return RedirectToPage("/Operations/Details", new { id = operationId });
+        return RedirectToPage( "/Operations/Details",
+                               new { id = operationId });
     }
 }
+////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
